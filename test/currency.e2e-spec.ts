5170ad93261173dc301f4e6250bd5bfd00b5e45b
Bruna Gois <bruna.gois@bitcapital.com.br>
2023-03-21 22:50:20 -0300

    fix: test

--- test/currency.e2e-spec.ts
+++ test/currency.e2e-spec.ts
@@ -3,18 +3,35 @@ import { INestApplication } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import request from 'supertest';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as sinon from 'sinon';
 import { CurrencyModule } from '../src/currencies/modules/currency.module';
 import { CurrencyRepository } from '../src/currencies/repositories/currency.repository';
+import { RatesService } from '@shared/external/rates.service';
 
 describe('CurrencyController (e2e)', () => {
   let app: INestApplication;
 
   let repository: CurrencyRepository;
 
+  const ratesServiceStub: {
+    feedRates: sinon.SinonStub;
+  } = {
+    feedRates: sinon.stub(),
+  };
+
   const deleteCreatedCurrency = async () => {
     const currency = await repository.findByCode('AOA');
 
     await repository.delete(currency.id);
+  };
+
+  const stubRatesService = async () => {
+    ratesServiceStub.feedRates = sinon
+      .stub(RatesService.prototype, 'feedRates')
+      .resolves({
+        rates: { BRL: 1, USD: 0.190738, EUR: 0.177043, INR: 15.767437 },
+        base: 'BRL',
+      });
   };
 
   beforeAll(async () => {
@@ -40,6 +57,8 @@ describe('CurrencyController (e2e)', () => {
 
     repository = app.get(CurrencyRepository);
 
+    await stubRatesService();
+
     await app.init();
   });
 
@@ -47,14 +66,14 @@ describe('CurrencyController (e2e)', () => {
     await deleteCreatedCurrency();
   });
 
-  it.skip('/currencies/conversion (GET)', async () => {
+  it('/currencies/conversion (GET)', async () => {
     const response = await request(app.getHttpServer())
       .get('/currencies/conversion')
       .query({
         code: 'BRL',
         value: 100,
       })
-      .expect('');
+      .expect(200);
 
     expect(response.body[0].code).toBe('USD');
     expect(response.body[0].value).toBeDefined();
